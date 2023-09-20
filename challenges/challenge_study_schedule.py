f07e9f46da6a92c4ea1f22ef58fc6194d9c1d34b
Ruan-Portella <ruanmorales29@gmail.com>
2023-09-20 14:39:18 -0300

    Feat: Refatora função e arruma linter

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,8 +1,6 @@
 def study_schedule(permanence_period, target_time):
     try:
         periods = 0
-        if target_time is None:
-            return None
         for i in permanence_period:
             if i[0] <= target_time <= i[1]:
                 periods += 1
