46c5990b0d4fbc60160739084eec1eec38151544
Ruan-Portella <ruanmorales29@gmail.com>
2023-09-20 14:31:24 -0300

    Feat: Requisito 02 Concluído

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,11 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message('Ruan Portella', 4) == 'alletroP _nauR'
+    assert encrypt_message('Ruan Portella', 30) == 'alletroP nauR'
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message('RuanP', 2.2)
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(2, 2)
