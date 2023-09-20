fd4d0cd63575f4c7f845a26b490b266fd8655bf9
Ruan-Portella <ruanmorales29@gmail.com>
2023-09-20 14:58:07 -0300

    Feat: Requisito 03 Concluído

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,18 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if (len(word) == 0):
+        return False
+    if (low_index == len(word)):
+        return False
+    if (high_index == 0):
+        return True
+    return verify_palindromo(word, low_index, high_index)
+
+
+def verify_palindromo(word, low_index, high_index):
+    if (low_index == high_index):
+        return True
+    if (word[low_index] != word[high_index]):
+        return False
+    if (low_index < high_index + 1):
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+    return True
