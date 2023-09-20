c299ea4cc0a540b11d84352a464e81702a78c636
Ruan-Portella <ruanmorales29@gmail.com>
2023-09-20 15:04:22 -0300

    Feat: Requisito 04 Concluído

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,11 @@
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    first_string = first_string.lower()
+    second_string = second_string.lower()
+    if len(first_string) != len(second_string):
+        return first_string, second_string, False
+    if len(first_string) < 1 and len(second_string) < 1:
+        return first_string, second_string, False
+    for letter in first_string:
+        if letter not in second_string:
+            return first_string, second_string, False
+    return first_string, second_string, True
