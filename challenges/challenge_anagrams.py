1880cd69bbb5eacc4ffb98143e4a233cf57a4cc7
Ruan-Portella <ruanmorales29@gmail.com>
2023-09-20 15:24:49 -0300

    Feat: Refatora Requisito 04

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,11 +1,18 @@
 def is_anagram(first_string, second_string):
-    first_string = first_string.lower()
-    second_string = second_string.lower()
-    if len(first_string) != len(second_string):
-        return first_string, second_string, False
     if len(first_string) < 1 and len(second_string) < 1:
+        return first_string.lower(), second_string.lower(), False
+    first_string = order_string(first_string)
+    second_string = order_string(second_string)
+    if not first_string == second_string:
         return first_string, second_string, False
-    for letter in first_string:
-        if letter not in second_string:
-            return first_string, second_string, False
     return first_string, second_string, True
+
+
+def order_string(string):
+    aplhabet = 'abcdefghijklmnopqrstuvwxyz'
+    string = string.lower()
+    ordered_string = ''
+    for letter in aplhabet:
+        if letter in string:
+            ordered_string += letter * string.count(letter)
+    return ordered_string
