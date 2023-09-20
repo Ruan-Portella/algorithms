bcb13c5b1340c41d1044d4c1a36ed72e6eb92dad
Ruan-Portella <ruanmorales29@gmail.com>
2023-09-20 14:18:11 -0300

    Feat: Requisito 01 Concluido

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    try:
+        periods = 0
+        if target_time is None:
+            return None
+        for i in permanence_period:
+            if i[0] <= target_time <= i[1]:
+                periods += 1
+        return periods
+    except (IndexError, TypeError, ValueError, AttributeError):
+        return None
