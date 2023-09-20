16becbc38d8cf84e19f85699fe916539469b7dc7
Ruan-Portella <ruanmorales29@gmail.com>
2023-09-20 15:42:05 -0300

    Feat: Refatora Requisito 06

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,8 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(word) == 0:
+        return False
+    wordReversed = word[::-1]
+    if word == wordReversed:
+        return True
+    else:
+        return False
