595e54f7300736793c3e43d78a3843273f65c6f1
Ruan-Portella <ruanmorales29@gmail.com>
2023-09-20 15:32:41 -0300

    Feat: Refatora Requisito 05

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,11 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not isinstance(nums, list) or len(nums) < 0:
+        return False
+    num_set = set()
+    for num in nums:
+        if not isinstance(num, int) or num < 1:
+            return False
+        if num in num_set:
+            return num
+        num_set.add(num)
+    return False
